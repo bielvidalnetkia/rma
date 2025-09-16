da13d1b949311db4e929ad3da047cee562c68687
Marie Lejeune <marie.lejeune@acsone.eu>
2025-09-16 12:27:15 +0200

    [MIG] rma_reason: Migration to 18.0

--- rma_reason/__manifest__.py
+++ rma_reason/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Rma Reason",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Raumschmiede GmbH,BCIM,ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/rma",
