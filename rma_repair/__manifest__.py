219d71eb61c14150b0ff97ff9ce1cca88a18059c
Víctor Martínez <victor.martinez@tecnativa.com>
2025-12-12 12:49:24 +0100

    [IMP] rma_repair: Define Repair Action in the Repair operation
    TT57882

--- rma_repair/__manifest__.py
+++ rma_repair/__manifest__.py
@@ -13,6 +13,7 @@
     "installable": True,
     "depends": ["rma_lot", "repair"],
     "data": [
+        "data/rma_operation_data.xml",
         "views/rma_operation.xml",
         "views/rma_views.xml",
         "views/repair_views.xml",
